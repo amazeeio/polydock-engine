77d12aad0af844060be038d6d6eb8920857d5d26
Bryan Gruneberg <hello@bryangruneberg.com>
2025-03-01 00:08:28 -0600

    Adding SRE Notifications

---
+++ lagoon/scripts/lagoon-post-rollout-end.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "Loading Lagoon environment"
+
+# Loading environment variables from .env and friends
+source /lagoon/entrypoints/50-dotenv.sh
+
+# Generate some additional enviornment variables
+source /lagoon/entrypoints/55-generate-env.sh
+
+echo "Done loading Lagoon environment"
+
+if [ ! -z "$POLYDOCK_SRE_SLACK_WEBHOOK_URL" ]; then
+    echo "[$SERVICE_NAME] Sending Slack notification"
+    curl -X POST -H 'Content-type: application/json' --data '{"text":":rocket:  [$SERVICE_NAME.$LAGOON_GIT_SAFE_BRANCH.$LAGOON_PROJECT] Post-rollout Ended"}' $POLYDOCK_SRE_SLACK_WEBHOOK_URL
+fi
--- lagoon/workers/start-horizon.sh
+++ lagoon/workers/start-horizon.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 
+echo "Loading Lagoon environment"
+
+# Loading environment variables from .env and friends
+source /lagoon/entrypoints/50-dotenv.sh
+
+# Generate some additional enviornment variables
+source /lagoon/entrypoints/55-generate-env.sh
+
+echo "Done loading Lagoon environment"
+
 # LAGOON_ENVIRONMENT_TYPE="TEST"
 
 if [ -f "/app/config/horizon.php" ]; then
+  
+  if [ ! -z "$POLYDOCK_SRE_SLACK_WEBHOOK_URL" ]; then
+    echo "[$SERVICE_NAME] Sending Slack notification"
+    curl -X POST -H 'Content-type: application/json' --data '{"text":":rocket: [$SERVICE_NAME] Horizon Started"}' $POLYDOCK_SRE_SLACK_WEBHOOK_URL
+  fi
+
   /usr/local/bin/php artisan horizon
 else
   echo "Horizon is not installed";
