b7bf9bb8a00cd32d0d6e0f5e34f4b4f51f82a0b9
Bryan Gruneberg <hello@bryangruneberg.com>
2025-02-28 23:24:07 -0600

    Adding SRE monitors for Horizon heartbeat

--- lagoon/workers/status-horizon.sh
+++ lagoon/workers/status-horizon.sh
@@ -1,14 +1,32 @@
 #!/bin/sh
 
-if [ -f "/app/config/horizon.php" ]; then
+echo "Loading Lagoon environment"
+
+# Loading environment variables from .env and friends
+source /lagoon/entrypoints/50-dotenv.sh
+
+# Generate some additional enviornment variables
+source /lagoon/entrypoints/55-generate-env.sh
+
+echo "Done loading Lagoon environment"
+
+if [ -z "$POLYDOCK_SRE_HORIZON_HEARTBEAT" ]; then
+  echo "[WARNING] - POLYDOCK_SRE_HORIZON_HEARTBEAT is not set"
+  exit 0
+fi
+
+if [ -f "config/horizon.php" ]; then
   COUNT=`ps ax | grep horizon:work | grep -v grep | wc -l`
 
   if [ $COUNT -gt 0 ]; then
-	echo Horizon is running
+	  echo "[INFO] - Horizon is running"
+    curl -XGET $POLYDOCK_SRE_HORIZON_HEARTBEAT
+    echo "--"
+    echo "[INFO] - Horizon heartbeat sent"
   else
-	echo Horizon is not running
+	  echo "[WARNING] - Horizon is not running"
   fi
 else
-  echo "[Status] - Horizon is not installed";
-  sleep 10
+  echo "[WARNING] - Horizon is not installed";
+  sleep 3
 fi
