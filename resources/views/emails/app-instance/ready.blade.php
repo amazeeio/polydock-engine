4ff82a75939202b5a5b607607251697956384f0c
Blaize Kaye <blaize.kaye@amazee.io>
2025-09-23 15:16:25 +1200

    Updates ready.blade to show direct url when hiding login info

--- resources/views/emails/app-instance/ready.blade.php
+++ resources/views/emails/app-instance/ready.blade.php
@@ -3,16 +3,26 @@ Hi {{ e($toUser->name) }},
 
 <h2 style="margin-bottom: 16px;">Your <em>"{{ $appInstance->storeApp->name }}"</em> Experience is now ready to use.</h2>
 
+@if($appInstance->getKeyValue('hide-login-email-info') != 'true')
 <x-mail::button :url="route('app-instances.show', $appInstance)">
     Access Your Experience
 </x-mail::button>
+@else
+<x-mail::button :url="$appInstance->app_url">
+    Access Your Experience
+</x-mail::button>
+@endif
 
 <h3 style="margin-top: 32px; margin-bottom: 8px;">Access Details:</h3>
 <ul style="margin-top: 0;">
 @if($appInstance->storeApp->trial_duration_days > 0)
     <li>Duration: {{ $appInstance->storeApp->trial_duration_days }} days</li>
 @endif
+@if($appInstance->getKeyValue('hide-login-email-info') != 'true')
     <li>Access URL: <a href="{{ route('app-instances.show', $appInstance) }}">{{ route('app-instances.show', $appInstance) }}</a></li>
+@else
+    <li>Access URL: <a href="{{ $appInstance->app_url }}">{{ $appInstance->app_url }}</a></li>
+@endif
 </ul>
 @if($appInstance->getKeyValue('hide-login-email-info') != 'true')
 <h3 style="margin-top: 32px; margin-bottom: 8px;">User Information & Login Credentials:</h3>
