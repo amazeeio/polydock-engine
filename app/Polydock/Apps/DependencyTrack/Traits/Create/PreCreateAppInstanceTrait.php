877c9d2bd71ba7596e44d123a8465e25369d4d92
Dan Lemon <daniel.lemon@amazee.io>
2026-07-17 18:41:37 +0200

    chore: migrate AmazeeClaw and DependencyTrack traits to runLifecyclePhase

--- app/Polydock/Apps/DependencyTrack/Traits/Create/PreCreateAppInstanceTrait.php
+++ app/Polydock/Apps/DependencyTrack/Traits/Create/PreCreateAppInstanceTrait.php
@@ -12,56 +12,44 @@ trait PreCreateAppInstanceTrait
     public function preCreateAppInstance(PolydockAppInstanceInterface $appInstance): PolydockAppInstanceInterface
     {
         $functionName = __FUNCTION__;
-        $logContext = $this->getLogContext($functionName);
-        $testLagoonPing = true;
+
         // Do not validate lagoon project name and ID because we are only injecting into a different project/org
-        $validateLagoonValues = true;
-        $validateLagoonProjectName = false;
-        $validateLagoonProjectId = false;
-
-        $this->info("$functionName: starting", $logContext);
-
-        $this->validateAppInstanceStatusIsExpectedAndConfigureLagoonClientAndVerifyLagoonValues(
+        return $this->runLifecyclePhase(
             $appInstance,
+            $functionName,
             PolydockAppInstanceStatus::PENDING_PRE_CREATE,
-            $logContext,
-            $testLagoonPing,
-            $validateLagoonValues,
-            $validateLagoonProjectName,
-            $validateLagoonProjectId
-        );
-
-        $this->info("$functionName: checking for Lagoon Organisation and Project fields", $logContext);
-
-        $appInstance->setStatus(
             PolydockAppInstanceStatus::PRE_CREATE_RUNNING,
-            PolydockAppInstanceStatus::PRE_CREATE_RUNNING->getStatusMessage()
-        )->save();
+            PolydockAppInstanceStatus::PRE_CREATE_COMPLETED,
+            PolydockAppInstanceStatus::PRE_CREATE_FAILED,
+            function (PolydockAppInstanceInterface $appInstance, array $logContext) use ($functionName): ?PolydockAppInstanceInterface {
+                $this->info("$functionName: checking for Lagoon Organisation and Project fields", $logContext);
 
-        $lagoonOrgName = $appInstance->getKeyValue('lagoon_organisation');
+                $lagoonOrgName = $appInstance->getKeyValue('lagoon_organisation');
 
-        try {
-            if (! empty($lagoonOrgName)) {
-                $this->info("$functionName: Found Lagoon Organisation setting, injecting variable LAGOON_FEATURE_FLAG_INSIGHTS.", $logContext);
-                $this->lagoonClient->addOrUpdateGlobalVariableForOrganization(
-                    $lagoonOrgName,
-                    'LAGOON_FEATURE_FLAG_INSIGHTS',
-                    'enabled'
-                );
-            }
-        } catch (\Exception $e) {
-            $this->error('Pre Create Failed: '.$e->getMessage(), [
-                'exception_class' => \get_class($e),
-                'exception_trace' => $e->getTraceAsString(),
-            ]);
-            $appInstance->setStatus(PolydockAppInstanceStatus::PRE_CREATE_FAILED, 'An exception occurred: '.$e->getMessage())->save();
+                try {
+                    if (! empty($lagoonOrgName)) {
+                        $this->info("$functionName: Found Lagoon Organisation setting, injecting variable LAGOON_FEATURE_FLAG_INSIGHTS.", $logContext);
+                        $this->lagoonClient->addOrUpdateGlobalVariableForOrganization(
+                            $lagoonOrgName,
+                            'LAGOON_FEATURE_FLAG_INSIGHTS',
+                            'enabled'
+                        );
+                    }
+                } catch (\Exception $e) {
+                    $this->error('Pre Create Failed: '.$e->getMessage(), [
+                        'exception_class' => \get_class($e),
+                        'exception_trace' => $e->getTraceAsString(),
+                    ]);
+                    $appInstance->setStatus(PolydockAppInstanceStatus::PRE_CREATE_FAILED, 'An exception occurred: '.$e->getMessage())->save();
 
-            return $appInstance;
-        }
+                    return $appInstance;
+                }
 
-        $this->info("$functionName: completed", $logContext);
-        $appInstance->setStatus(PolydockAppInstanceStatus::PRE_CREATE_COMPLETED, 'Pre-create completed')->save();
-
-        return $appInstance;
+                return null;
+            },
+            'Pre-create completed',
+            validateLagoonProjectName: false,
+            validateLagoonProjectId: false,
+        );
     }
 }
